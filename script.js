e8351e8135caf4093e2e22882c7a89a36ecd922d
perencee26 <aasuprince.kumar@gmail.com>
2024-07-15 23:22:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".changelines",{
+    strings: ["Sahana Arumugam.","Sahanaa kuttyy.","Sahana Mineee."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true 
+})
